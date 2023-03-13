34984440b006b60ee43f0301568f154122c7960d
ProCode2 <iamprobablycoding@gmail.com>
2023-03-13 16:38:00 +0530

    fix

--- utils/db/index.js
+++ utils/db/index.js
@@ -1,10 +1,15 @@
 import admin from "firebase-admin";
 import ServiceAccount from "../../config/serviceProd.json";
-console.log(ServiceAccount);
+import ServiceAccountDev from "../../config/serviceAccountKey.json";
+
 if (!admin.apps.length) {
   try {
     admin.initializeApp({
-      credential: admin.credential.cert(ServiceAccount),
+      credential: admin.credential.cert(
+        process.env.ENVIRONMENT == "production"
+          ? ServiceAccount
+          : ServiceAccountDev
+      ),
     });
   } catch (error) {
     console.log("Firebase admin initialization error", error.stack);
