8fcce6856c854a6255b523c2321495a998dafe64
ProCode2 <iamprobablycoding@gmail.com>
2023-03-13 06:32:01 +0530

    firebase fix

--- firebase.config.js
+++ firebase.config.js
@@ -2,16 +2,32 @@ import { initializeApp, getApps, getApp } from "firebase/app";
 import { getFirestore } from "firebase/firestore";
 import { getStorage } from "firebase/storage";
 
-const firebaseConfig = {
-  apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
-  authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTHA_DOMAIN,
-  databaseURL: process.env.NEXT_PUBLIC_FIREBASE_DATABASE_URL,
-  projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
-  storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCCKET,
-  messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSEAGING_ID,
-  appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID,
-  measurementId: process.env.NEXT_PUBLIC_FIREBASE_MEASURMENT_ID,
+const firebaseDevConfig = {
+  apiKey: process.env.NEXT_PUBLIC_DEV_FIREBASE_API_KEY,
+  authDomain: process.env.NEXT_PUBLIC_DEV_FIREBASE_AUTHA_DOMAIN,
+  databaseURL: process.env.NEXT_PUBLIC_DEV_FIREBASE_DATABASE_URL,
+  projectId: process.env.NEXT_PUBLIC_DEV_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.NEXT_PUBLIC_DEV_FIREBASE_STORAGE_BUCCKET,
+  messagingSenderId: process.env.NEXT_PUBLIC_DEV_FIREBASE_MESSEAGING_ID,
+  appId: process.env.NEXT_PUBLIC_DEV_FIREBASE_APP_ID,
+  measurementId: process.env.NEXT_PUBLIC_DEV_FIREBASE_MEASURMENT_ID,
 };
+
+const firebaseProdConfig = {
+  apiKey: process.env.NEXT_PUBLIC_PROD_FIREBASE_API_KEY,
+  authDomain: process.env.NEXT_PUBLIC_PROD_FIREBASE_AUTHA_DOMAIN,
+  databaseURL: process.env.NEXT_PUBLIC_PROD_FIREBASE_DATABASE_URL,
+  projectId: process.env.NEXT_PUBLIC_PROD_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.NEXT_PUBLIC_PROD_FIREBASE_STORAGE_BUCCKET,
+  messagingSenderId: process.env.NEXT_PUBLIC_PROD_FIREBASE_MESSEAGING_ID,
+  appId: process.env.NEXT_PUBLIC_PROD_FIREBASE_APP_ID,
+  measurementId: process.env.NEXT_PUBLIC_PROD_FIREBASE_MEASURMENT_ID,
+};
+
+const firebaseConfig =
+  process.env.ENVIRONMENT == "production"
+    ? firebaseProdConfig
+    : firebaseDevConfig;
 
 const app = getApps().length > 0 ? getApp() : initializeApp(firebaseConfig);
 
